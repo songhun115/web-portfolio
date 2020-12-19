8b3367c538b8ef2315bbaa32453f7bf333e1aec7
songhun115 <hoho77113@daum.net>
2020-12-19 19:27:20 +0900

    메인 페이지 반응형 마무리 및 section1 슬라이더 content마무리

--- main.js
+++ main.js
@@ -5,8 +5,32 @@ $(document).ready(function () {
             $clickd.removeClass('active');
         } else {
             $clickd.addClass('active');  
-        }
+        }   
     })
+    const navbarMenu = document.querySelector('.navbar__menu_list');
+    navbarMenu.addEventListener('click',(event) => {
+      const target = event.target;
+      const link = target.dataset.link;
+      if(link == null){
+        return;
+      }
+      
+      const scrollTo = document.querySelector(link);
+      scrollTo.scrollIntoView({behavior:"smooth"});
+    });
+
+    const navbtnMenu = document.querySelector('.menu_list');
+    navbtnMenu.addEventListener('click',(event) => {
+      const target = event.target;
+      const link = target.dataset.link;
+      if(link == null){
+        return;
+      }
+      
+      const scrollTo = document.querySelector(link);
+      scrollTo.scrollIntoView({behavior:"smooth"});
+    });
+ 
 });
 
 
